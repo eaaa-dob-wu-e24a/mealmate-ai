ccf156dc54cc502d1d02fd353490a6cce34d2c1d
Aloushi755 <alimoussa755@hotmail.com>
2025-02-17 19:44:23 +0100

    Created the navbar for the project

---
+++ remix/app/layouts/navbar.tsx
@@ -0,0 +1,43 @@
+import type { Route } from "./+types/navbar";
+import { NavLink } from "react-router";
+
+import { IoCalendarClearOutline } from "react-icons/io5";
+import { FiHome } from "react-icons/fi";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { HiOutlineUsers } from "react-icons/hi2";
+
+import { FaRegBookmark } from "react-icons/fa";
+
+export default function BottomNav() {
+  const navItems = [
+    { icon: <FiHome size={24} />, to: "/" },
+    { icon: <IoCalendarClearOutline size={24} />, to: "/calendar" },
+    {
+      icon: <HiOutlineUsers size={28} className="text-green-700" />,
+      to: "/users",
+    },
+    { icon: <FaRegBookmark size={24} />, to: "/bookmarks" },
+    { icon: <FaRegCircleUser size={24} />, to: "/profile" },
+  ];
+
+  return (
+    <nav className="fixed bottom-0 left-0 w-full bg-[#F8F5EF] py-3 border-t border-gray-300">
+      <ul className="flex justify-around items-center max-w-md mx-auto list-none p-0 m-0">
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <NavLink
+              to={item.to}
+              className={({ isActive }) =>
+                `flex items-center justify-center p-2 ${
+                  isActive ? "text-green-700" : "text-gray-600"
+                }`
+              }
+            >
+              {item.icon}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
--- remix/app/routes.ts
+++ remix/app/routes.ts
@@ -1,9 +1,16 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("welcome", "routes/welcome.tsx"),
-  route("onboarding", "routes/onboarding.tsx"),
-  route("/chatbot", "routes/chatbot.tsx"),
-  route("login", "routes/login.tsx"),
+  layout("layouts/navbar.tsx", [
+    index("routes/home.tsx"),
+    route("welcome", "routes/welcome.tsx"),
+    route("onboarding", "routes/onboarding.tsx"),
+    route("/chatbot", "routes/chatbot.tsx"),
+    route("login", "routes/login.tsx"),
+  ]),
 ] satisfies RouteConfig;
