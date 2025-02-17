77131f3167c38df2415bfd2e659e88fa1c0eca93
Nicolai Harms <Nicolai.harms@gmail.com>
2025-02-17 14:03:14 +0100

    Updated styling for Welcome and login page

--- remix/app/routes/login.tsx
+++ remix/app/routes/login.tsx
@@ -1,3 +1,30 @@
+import { Button, buttonVariants } from "~/components/ui/button";
+import { Link } from "react-router";
+import { cn } from "~/lib/utils";
+import { Input } from "~/components/ui/input";
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="relative bg-cover h-screen flex flex-col  items-center justify-center bg-center bg-no-repeat bg-[url('/img/loginhero.jpg')]">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      {/* Content */}
+      <div className="relative z-10 text-center flex flex-col justify-center gap-4 h-full">
+        <h1 className="text-white text-4xl font-bold">Login to your account</h1>
+        <p className="text-white text-2xl">
+          Enter your details below to login to your account
+        </p>
+        <div className="flex flex-col gap-4 absolute bottom-4 w-full  ">
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+          <Link
+            to="/onboarding"
+            className={cn(buttonVariants({ variant: "default", size: "lg" }))}
+          >
+            Create Account
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- remix/app/routes/welcome.tsx
+++ remix/app/routes/welcome.tsx
@@ -1,13 +1,34 @@
-import React from "react";
 import { Link } from "react-router";
+import { buttonVariants } from "../components/ui/button";
+import { cn } from "../lib/utils";
 
 const WelcomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Our Application!</h1>
-      <p>Please choose an option:</p>
-      <Link to="/login">Login</Link>
-      <Link to="/onboarding">Create Account</Link>
+    <div className="relative bg-cover h-screen flex flex-col  items-center justify-center bg-center bg-no-repeat bg-[url('/img/welcomehero.jpg')]">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      {/* Content */}
+      <div className="relative z-10 text-center flex flex-col justify-center gap-4 h-full">
+        <h1 className="text-white text-4xl font-bold">
+          Welcome to Our Application!
+        </h1>
+        <p className="text-white text-2xl">Please choose an option:</p>
+        <div className="flex flex-col gap-4 absolute bottom-4 w-full  ">
+          <Link
+            to="/login"
+            className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+          >
+            Login
+          </Link>
+          <Link
+            to="/onboarding"
+            className={cn(buttonVariants({ variant: "default", size: "lg" }))}
+          >
+            Create Account
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
