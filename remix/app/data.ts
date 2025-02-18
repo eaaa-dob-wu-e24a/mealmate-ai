09d9882bb5c0198b9d5452b751e9a795db69e3a3
Nicolai Harms <Nicolai.harms@gmail.com>
2025-02-18 11:23:52 +0100

    Card UI for archive recipes

--- remix/app/data.ts
+++ remix/app/data.ts
@@ -1,345 +1,342 @@
-{
-  ("recipes");
-  [
-    {
-      title: "Tomato Lentils Spaghetti",
-      image: "image_url_1",
-      servings: 4,
-      preparation: {
-        total_time: "40 min",
-        prep_time: "15 min",
-        cook_time: "25 min",
-      },
-      ingredients: [
-        {
-          name: "Dried red or brown lentils",
-          quantity: { amount: 50, unit: "g" },
-        },
-        {
-          name: "Spaghetti",
-          quantity: { amount: 200, unit: "g" },
-        },
-        {
-          name: "Tomato sauce",
-          quantity: { amount: 240, unit: "mL" },
-        },
-        {
-          name: "Parmesan cheese",
-          quantity: { amount: 25, unit: "g" },
-        },
-        {
-          name: "Garlic",
-          quantity: { amount: 2, unit: "cloves" },
-        },
-      ],
-      allergies: ["Gluten", "Dairy"],
-      categories: ["Italian", "Vegetarian", "Quick & Easy"],
+export const recipes = [
+  {
+    title: "Tomato Lentils Spaghetti",
+    image: "/img/recipe-mock/recipe1.jpg",
+    servings: 4,
+    preparation: {
+      total_time: "40 min",
+      prep_time: "15 min",
+      cook_time: "25 min",
     },
-    {
-      title: "Avocado Toast",
-      image: "image_url_2",
-      servings: 4,
-      preparation: {
-        total_time: "10 min",
-        prep_time: "5 min",
-        cook_time: "5 min",
+    ingredients: [
+      {
+        name: "Dried red or brown lentils",
+        quantity: { amount: 50, unit: "g" },
       },
-      ingredients: [
-        {
-          name: "Bread",
-          quantity: { amount: 2, unit: "slices" },
-        },
-        {
-          name: "Avocado",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Lemon juice",
-          quantity: { amount: 5, unit: "mL" },
-        },
-        {
-          name: "Salt",
-          quantity: { amount: "to taste", unit: "" },
-        },
-        {
-          name: "Pepper",
-          quantity: { amount: "to taste", unit: "" },
-        },
-      ],
-      allergies: ["Gluten"],
-      categories: ["Quick & Easy", "Vegan", "Vegetarian"],
-    },
-    {
-      title: "Mango Smoothie",
-      image: "image_url_3",
-      servings: 4,
-      preparation: {
-        total_time: "5 min",
-        prep_time: "5 min",
-        cook_time: "0 min",
+      {
+        name: "Spaghetti",
+        quantity: { amount: 200, unit: "g" },
       },
-      ingredients: [
-        {
-          name: "Mango",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Banana",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Greek yogurt",
-          quantity: { amount: 120, unit: "mL" },
-        },
-        {
-          name: "Milk",
-          quantity: { amount: 240, unit: "mL" },
-        },
-        {
-          name: "Honey",
-          quantity: { amount: 15, unit: "mL" },
-        },
-      ],
-      allergies: ["Dairy"],
-      categories: ["Quick & Easy", "Vegetarian"],
-    },
-    {
-      title: "Grilled Chicken Salad",
-      image: "image_url_4",
-      servings: 4,
-      preparation: {
-        total_time: "20 min",
-        prep_time: "10 min",
-        cook_time: "10 min",
+      {
+        name: "Tomato sauce",
+        quantity: { amount: 240, unit: "mL" },
       },
-      ingredients: [
-        {
-          name: "Chicken breast",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Lettuce",
-          quantity: { amount: 50, unit: "g" },
-        },
-        {
-          name: "Cherry tomatoes",
-          quantity: { amount: 75, unit: "g" },
-        },
-        {
-          name: "Cucumber",
-          quantity: { amount: 0.5, unit: "whole" },
-        },
-        {
-          name: "Olive oil",
-          quantity: { amount: 15, unit: "mL" },
-        },
-      ],
-      allergies: [],
-      categories: ["Gluten-Free", "Quick & Easy", "Mediterranean"],
-    },
-    {
-      title: "Oatmeal Breakfast Bowl",
-      image: "image_url_5",
-      servings: 4,
-      preparation: {
-        total_time: "10 min",
-        prep_time: "5 min",
-        cook_time: "5 min",
+      {
+        name: "Parmesan cheese",
+        quantity: { amount: 25, unit: "g" },
       },
-      ingredients: [
-        {
-          name: "Oats",
-          quantity: { amount: 45, unit: "g" },
-        },
-        {
-          name: "Milk",
-          quantity: { amount: 240, unit: "mL" },
-        },
-        {
-          name: "Honey",
-          quantity: { amount: 15, unit: "mL" },
-        },
-        {
-          name: "Banana",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Almonds",
-          quantity: { amount: 16, unit: "g" },
-        },
-      ],
-      allergies: ["Dairy", "Nuts"],
-      categories: ["Vegetarian", "Quick & Easy"],
-    },
-    {
-      title: "Pancakes",
-      image: "image_url_6",
-      servings: 4,
-      preparation: {
-        total_time: "20 min",
-        prep_time: "10 min",
-        cook_time: "10 min",
+      {
+        name: "Garlic",
+        quantity: { amount: 2, unit: "cloves" },
       },
-      ingredients: [
-        {
-          name: "Flour",
-          quantity: { amount: 125, unit: "g" },
-        },
-        {
-          name: "Milk",
-          quantity: { amount: 240, unit: "mL" },
-        },
-        {
-          name: "Egg",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Baking powder",
-          quantity: { amount: 5, unit: "mL" },
-        },
-        {
-          name: "Butter",
-          quantity: { amount: 30, unit: "g" },
-        },
-      ],
-      allergies: ["Gluten", "Dairy", "Egg"],
-      categories: ["Quick & Easy", "Desserts", "Vegetarian"],
+    ],
+    allergies: ["Gluten", "Dairy"],
+    categories: ["Italian", "Vegetarian", "Quick & Easy"],
+  },
+  {
+    title: "Avocado Toast",
+    image: "/img/recipe-mock/recipe2.jpg",
+    servings: 4,
+    preparation: {
+      total_time: "10 min",
+      prep_time: "5 min",
+      cook_time: "5 min",
     },
-    {
-      title: "Vegetable Stir Fry",
-      image: "image_url_7",
-      servings: 4,
-      preparation: {
-        total_time: "25 min",
-        prep_time: "10 min",
-        cook_time: "15 min",
+    ingredients: [
+      {
+        name: "Bread",
+        quantity: { amount: 2, unit: "slices" },
       },
-      ingredients: [
-        {
-          name: "Bell pepper",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Carrot",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Broccoli",
-          quantity: { amount: 50, unit: "g" },
-        },
-        {
-          name: "Soy sauce",
-          quantity: { amount: 30, unit: "mL" },
-        },
-        {
-          name: "Olive oil",
-          quantity: { amount: 15, unit: "mL" },
-        },
-      ],
-      allergies: ["Soy", "Gluten"],
-      categories: ["Chinese", "Vegan", "Vegetarian", "Quick & Easy"],
-    },
-    {
-      title: "Garlic Butter Shrimp",
-      image: "image_url_8",
-      servings: 4,
-      preparation: {
-        total_time: "15 min",
-        prep_time: "5 min",
-        cook_time: "10 min",
+      {
+        name: "Avocado",
+        quantity: { amount: 1, unit: "" },
       },
-      ingredients: [
-        {
-          name: "Shrimp",
-          quantity: { amount: 200, unit: "g" },
-        },
-        {
-          name: "Garlic",
-          quantity: { amount: 2, unit: "cloves" },
-        },
-        {
-          name: "Butter",
-          quantity: { amount: 30, unit: "g" },
-        },
-        {
-          name: "Parsley",
-          quantity: { amount: 3, unit: "g" },
-        },
-        {
-          name: "Lemon juice",
-          quantity: { amount: 15, unit: "mL" },
-        },
-      ],
-      allergies: ["Shellfish", "Dairy"],
-      categories: ["Quick & Easy", "Fine Dining"],
-    },
-    {
-      title: "Homemade Pizza",
-      image: "image_url_9",
-      servings: 4,
-      preparation: {
-        total_time: "1 hour",
-        prep_time: "30 min",
-        cook_time: "30 min",
+      {
+        name: "Lemon juice",
+        quantity: { amount: 5, unit: "mL" },
       },
-      ingredients: [
-        {
-          name: "Pizza dough",
-          quantity: { amount: 1, unit: "" },
-        },
-        {
-          name: "Tomato sauce",
-          quantity: { amount: 120, unit: "mL" },
-        },
-        {
-          name: "Mozzarella cheese",
-          quantity: { amount: 60, unit: "g" },
-        },
-        {
-          name: "Pepperoni",
-          quantity: { amount: 30, unit: "g" },
-        },
-        {
-          name: "Olive oil",
-          quantity: { amount: 15, unit: "mL" },
-        },
-      ],
-      allergies: ["Gluten", "Dairy"],
-      categories: ["Italian", "Quick & Easy"],
-    },
-    {
-      title: "Chocolate Chip Cookies",
-      image: "image_url_10",
-      servings: 4,
-      preparation: {
-        total_time: "30 min",
-        prep_time: "10 min",
-        cook_time: "20 min",
+      {
+        name: "Salt",
+        quantity: { amount: "to taste", unit: "" },
       },
-      ingredients: [
-        {
-          name: "Flour",
-          quantity: { amount: 125, unit: "g" },
-        },
-        {
-          name: "Butter",
-          quantity: { amount: 115, unit: "g" },
-        },
-        {
-          name: "Sugar",
-          quantity: { amount: 100, unit: "g" },
-        },
-        {
-          name: "Chocolate chips",
-          quantity: { amount: 85, unit: "g" },
-        },
-        {
-          name: "Egg",
-          quantity: { amount: 1, unit: "" },
-        },
-      ],
-      allergies: ["Gluten", "Dairy", "Egg"],
-      categories: ["Desserts", "Quick & Easy", "Vegetarian"],
+      {
+        name: "Pepper",
+        quantity: { amount: "to taste", unit: "" },
+      },
+    ],
+    allergies: ["Gluten"],
+    categories: ["Quick & Easy", "Vegan", "Vegetarian"],
+  },
+  {
+    title: "Mango Smoothie",
+    image: "/img/recipe-mock/recipe3.jpg",
+    servings: 4,
+    preparation: {
+      total_time: "5 min",
+      prep_time: "5 min",
+      cook_time: "0 min",
     },
-  ];
-}
+    ingredients: [
+      {
+        name: "Mango",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Banana",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Greek yogurt",
+        quantity: { amount: 120, unit: "mL" },
+      },
+      {
+        name: "Milk",
+        quantity: { amount: 240, unit: "mL" },
+      },
+      {
+        name: "Honey",
+        quantity: { amount: 15, unit: "mL" },
+      },
+    ],
+    allergies: ["Dairy"],
+    categories: ["Quick & Easy", "Vegetarian"],
+  },
+  {
+    title: "Grilled Chicken Salad",
+    image: "image_url_4",
+    servings: 4,
+    preparation: {
+      total_time: "20 min",
+      prep_time: "10 min",
+      cook_time: "10 min",
+    },
+    ingredients: [
+      {
+        name: "Chicken breast",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Lettuce",
+        quantity: { amount: 50, unit: "g" },
+      },
+      {
+        name: "Cherry tomatoes",
+        quantity: { amount: 75, unit: "g" },
+      },
+      {
+        name: "Cucumber",
+        quantity: { amount: 0.5, unit: "whole" },
+      },
+      {
+        name: "Olive oil",
+        quantity: { amount: 15, unit: "mL" },
+      },
+    ],
+    allergies: [],
+    categories: ["Gluten-Free", "Quick & Easy", "Mediterranean"],
+  },
+  {
+    title: "Oatmeal Breakfast Bowl",
+    image: "image_url_5",
+    servings: 4,
+    preparation: {
+      total_time: "10 min",
+      prep_time: "5 min",
+      cook_time: "5 min",
+    },
+    ingredients: [
+      {
+        name: "Oats",
+        quantity: { amount: 45, unit: "g" },
+      },
+      {
+        name: "Milk",
+        quantity: { amount: 240, unit: "mL" },
+      },
+      {
+        name: "Honey",
+        quantity: { amount: 15, unit: "mL" },
+      },
+      {
+        name: "Banana",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Almonds",
+        quantity: { amount: 16, unit: "g" },
+      },
+    ],
+    allergies: ["Dairy", "Nuts"],
+    categories: ["Vegetarian", "Quick & Easy"],
+  },
+  {
+    title: "Pancakes",
+    image: "image_url_6",
+    servings: 4,
+    preparation: {
+      total_time: "20 min",
+      prep_time: "10 min",
+      cook_time: "10 min",
+    },
+    ingredients: [
+      {
+        name: "Flour",
+        quantity: { amount: 125, unit: "g" },
+      },
+      {
+        name: "Milk",
+        quantity: { amount: 240, unit: "mL" },
+      },
+      {
+        name: "Egg",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Baking powder",
+        quantity: { amount: 5, unit: "mL" },
+      },
+      {
+        name: "Butter",
+        quantity: { amount: 30, unit: "g" },
+      },
+    ],
+    allergies: ["Gluten", "Dairy", "Egg"],
+    categories: ["Quick & Easy", "Desserts", "Vegetarian"],
+  },
+  {
+    title: "Vegetable Stir Fry",
+    image: "image_url_7",
+    servings: 4,
+    preparation: {
+      total_time: "25 min",
+      prep_time: "10 min",
+      cook_time: "15 min",
+    },
+    ingredients: [
+      {
+        name: "Bell pepper",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Carrot",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Broccoli",
+        quantity: { amount: 50, unit: "g" },
+      },
+      {
+        name: "Soy sauce",
+        quantity: { amount: 30, unit: "mL" },
+      },
+      {
+        name: "Olive oil",
+        quantity: { amount: 15, unit: "mL" },
+      },
+    ],
+    allergies: ["Soy", "Gluten"],
+    categories: ["Chinese", "Vegan", "Vegetarian", "Quick & Easy"],
+  },
+  {
+    title: "Garlic Butter Shrimp",
+    image: "image_url_8",
+    servings: 4,
+    preparation: {
+      total_time: "15 min",
+      prep_time: "5 min",
+      cook_time: "10 min",
+    },
+    ingredients: [
+      {
+        name: "Shrimp",
+        quantity: { amount: 200, unit: "g" },
+      },
+      {
+        name: "Garlic",
+        quantity: { amount: 2, unit: "cloves" },
+      },
+      {
+        name: "Butter",
+        quantity: { amount: 30, unit: "g" },
+      },
+      {
+        name: "Parsley",
+        quantity: { amount: 3, unit: "g" },
+      },
+      {
+        name: "Lemon juice",
+        quantity: { amount: 15, unit: "mL" },
+      },
+    ],
+    allergies: ["Shellfish", "Dairy"],
+    categories: ["Quick & Easy", "Fine Dining"],
+  },
+  {
+    title: "Homemade Pizza",
+    image: "image_url_9",
+    servings: 4,
+    preparation: {
+      total_time: "1 hour",
+      prep_time: "30 min",
+      cook_time: "30 min",
+    },
+    ingredients: [
+      {
+        name: "Pizza dough",
+        quantity: { amount: 1, unit: "" },
+      },
+      {
+        name: "Tomato sauce",
+        quantity: { amount: 120, unit: "mL" },
+      },
+      {
+        name: "Mozzarella cheese",
+        quantity: { amount: 60, unit: "g" },
+      },
+      {
+        name: "Pepperoni",
+        quantity: { amount: 30, unit: "g" },
+      },
+      {
+        name: "Olive oil",
+        quantity: { amount: 15, unit: "mL" },
+      },
+    ],
+    allergies: ["Gluten", "Dairy"],
+    categories: ["Italian", "Quick & Easy"],
+  },
+  {
+    title: "Chocolate Chip Cookies",
+    image: "image_url_10",
+    servings: 4,
+    preparation: {
+      total_time: "30 min",
+      prep_time: "10 min",
+      cook_time: "20 min",
+    },
+    ingredients: [
+      {
+        name: "Flour",
+        quantity: { amount: 125, unit: "g" },
+      },
+      {
+        name: "Butter",
+        quantity: { amount: 115, unit: "g" },
+      },
+      {
+        name: "Sugar",
+        quantity: { amount: 100, unit: "g" },
+      },
+      {
+        name: "Chocolate chips",
+        quantity: { amount: 85, unit: "g" },
+      },
+      {
+        name: "Egg",
+        quantity: { amount: 1, unit: "" },
+      },
+    ],
+    allergies: ["Gluten", "Dairy", "Egg"],
+    categories: ["Desserts", "Quick & Easy", "Vegetarian"],
+  },
+];
