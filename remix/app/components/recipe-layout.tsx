1d9e3f29d3ffe2d3ecd40d4f317a1882da60ed40
Aloushi755 <alimoussa755@hotmail.com>
2025-02-19 13:55:23 +0100

    Added the seach bar to the recipe archive.

--- remix/app/components/recipe-layout.tsx
+++ remix/app/components/recipe-layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 import Recipe from "./recipe";
+import RecipeSearch from "./search";
 
 type Recipe = {
   _id: string;
@@ -21,6 +22,9 @@ export default function RecipeLayout({
   return (
     <div className="mx-auto">
       <h1 className="text-2xl font-bold mb-6">{title}</h1>
+      <div className="pb-4">
+        <RecipeSearch recipes={recipes} />
+      </div>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-4 lg:grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
         {recipes.length > 0 ? (
           recipes.map((recipe, index) => (
