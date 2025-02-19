2db29026b3614755b716450ceccd2fc0cc3ed8f6
Nicolai Harms <Nicolai.harms@gmail.com>
2025-02-19 13:24:45 +0100

    Update user functionality and small updates

--- remix/app/components/recipe-layout.tsx
+++ remix/app/components/recipe-layout.tsx
@@ -21,7 +21,7 @@ export default function RecipeLayout({
   return (
     <div className="mx-auto">
       <h1 className="text-2xl font-bold mb-6">{title}</h1>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(150px,1fr))] gap-4 lg:grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(130px,1fr))] gap-4 ">
         {recipes.length > 0 ? (
           recipes.map((recipe, index) => (
             <Recipe
