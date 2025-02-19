8309dee1f4ee12d31c4bc5aa8d1593e4af65f007
Nicolai Harms <Nicolai.harms@gmail.com>
2025-02-19 14:07:11 +0100

    prepared for atlas hosting

--- node/src/database.ts
+++ node/src/database.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, USE_ATLAS } =
+  process.env;
 
 const connectDB = async () => {
   try {
-    const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+    const mongoURI =
+      USE_ATLAS === "true"
+        ? `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/${DB_NAME}`
+        : `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
     await mongoose.connect(mongoURI);
     if (process.env.NODE_ENV !== "development") {
       mongoose.set("overwriteModels", true);
